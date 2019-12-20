a782204cbb6a75d7b6e33ed1a4e38c5163fc5de5
Conary36 <cbeckford144@gmail.com>
2019-12-20 14:32:53 -0500

    made changes to usercard

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,19 +1,21 @@
 import React from "react";
 
-export default function UserCard({url, names, local, product, price}){
+export default function UserCard({id, username, password, owner_first_name, owner_last_name,business_name,avatar}){
 
 
 
     return(
         <section>
             <div>
-                <img src={url} alt="merchant"/>
+                <img src={avatar} alt="merchant"/>
             </div>
             <div>
-                <h1>Name: {names}</h1>
-                <p>Location: {local}</p>
-                <p>Product: {product}</p>
-                <p>Price: {price}</p>
+                <h1>ID:{id}</h1>
+                <p>Username: {username}</p>
+                <p>password: {password}</p>
+                <p>fname: {owner_first_name}</p>
+                <p>lname:{owner_last_name}</p>
+                <p>Business: {business_name}</p>
             </div>
         </section>
 
